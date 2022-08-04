3928fea1a1b3b70138573e5529ceead703d0ecc5
Luciana Corti <lulicorti19@gmail.com>
2022-08-04 19:30:23 -0300

    modifique el desafio n 2

--- js/ciclos.js
+++ js/ciclos.js
@@ -1,29 +1,69 @@
-// Calculador de cuotas
-let valorProducto = 0
-let cuota = 0
-let valorCuota = 0
+///Tienda Online
 
-debugger
-alert ("Calcule las cuotas de su compra!")
-valorProducto = parseFloat(prompt("Ingresa el valor del producto adquirido:"))
-cuota = parseFloat(prompt("Ingresa la cantidad de cuotas que desea: 1, 3, 6, 12 o 18"))
-valorCuota = calcular(valorProducto, cuota)
-console.log("Valor de la cuota según la cantidad seleccionada:$", valorCuota)
-alert ("Valor de la cuota según la cantidad seleccionada:$" + " " + valorCuota)
+function ingresoDatos() {
+  alert("Bienvenida/o a la tienda online Bali");
+  let nombreUsuario = prompt("Ingrese su nombre");
+  while (nombreUsuario === "") {
+    nombreUsuario = prompt("Para continuar debe ingresar su nombre:");
+  }
+}
 
-function calcular(valorProducto, cuota) {
-switch (cuota) {
-    case 1:
-        return valorProducto
-    case 3:
-        return valorProducto / 3
-    case 6:
-        return valorProducto / 6
-    case 12:
-        return valorProducto / 12
-    case 18:
-        return valorProducto / 18
-    default:
-        return "Incorrecto, vuelva a intentarlo"
+function catalogoProductos() {
+  let productoCatalogo;
+  do {
+    productoCatalogo = prompt(
+      " Productos disponibles: 1- Vela aroma vainilla  2- Sahumerios aroma coco  3- Difusor aroma lima  4- Humidificador"
+    );
+  } while (
+    productoCatalogo != 1 &&
+    productoCatalogo != 2 &&
+    productoCatalogo != 3 &&
+    productoCatalogo != 4
+  );
+  switch (productoCatalogo) {
+    case "1":
+      return "Vela aroma vainilla";
+    case "2":
+      return "Sahumerios aroma coco";
+    case "3":
+      return "Difusor aroma lima";
+    case "4":
+      return "Humidificador";
+  }
 }
+
+function mostrarPrecio(producto) {
+  if (producto == "Vela aroma vainilla") {
+    return 800;
+  } else if (producto == "Sahumerios aroma coco") {
+    return 650;
+  } else if (producto == "Difusor aroma lima") {
+    return 780;
+  } else if (producto == "Humidificador") {
+    return 1200;
+  }
 }
+
+function cobroFinal(producto, precio) {
+  alert(
+    "Seleccionó el siguiente producto : " +
+      producto.toLowerCase() +
+      "Precio $ " +
+      precio
+  );
+  let pago = prompt("Con cuánto va a abonar ? ");
+  if (pago > precio) {
+    alert(
+      "Su vuelto es $" + (pago - precio) + ",Muchas gracias por su compra!"
+    );
+  } else if (pago == precio) {
+    alert("Muchas gracias por abonar exacto!");
+  }
+  if (pago < precio)
+    alert("Quitaremos el producto de tu carrito por falta de pago!");
+}
+
+ingresoDatos();
+let productoSeleccionado = catalogoProductos();
+let precioProducto = mostrarPrecio(productoSeleccionado);
+cobroFinal(productoSeleccionado, precioProducto);
