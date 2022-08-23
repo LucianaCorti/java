29e08f78c6306856dc6de3a3af5e0984e91b0b03
Luciana Corti <lulicorti19@gmail.com>
2022-08-23 19:38:30 -0300

    Agregue funcionalidad a los botones

--- js/dom.js
+++ js/dom.js
@@ -116,7 +116,7 @@ function crearTablaProductos() {
       <h5 class="card-title text-decoration-underline">${producto.nombre}</h5>
       <p class="card-text">${producto.descripcion}</p>
       <p class="card-text">$${producto.precio}</p>
-      <button type="button" class="btn btn-dark m-3 fs-6 " id="btnCards${producto.id}">Añadir al carrito</button>
+      <button type="button" class="btn btn-dark m-3 fs-6" id="btnCards${producto.id}">Añadir al carrito</button>
     </div>
     </div>
     `;
@@ -131,7 +131,6 @@ function funcionBotones() {
     document
       .querySelector(`#btnCards${producto.id}`)
       .addEventListener("click", () => {
-        console.log(producto);
         carrito(producto);
       });
   });
@@ -149,6 +148,41 @@ function carrito(producto) {
     prod2.cantidad++;
   }
   console.log(carritoCompras);
+  agregarAlCarrito();
+}
+
+
+const carritoProductos = document.querySelector(".carritoCompras");
+
+function agregarAlCarrito() {
+  carritoProductos.innerHTML = "";
+  carritoCompras.forEach((producto) => {
+    carritoProductos.innerHTML += `<div class="card">
+    <h5 class="card-title text-decoration-underline m-2">Carrito Compras</h5>
+  <img class="card-img-top" src=${producto.foto}>
+  <div class="card-body">
+    <h5 class="card-title text-decoration-underline">${producto.nombre}</h5>
+    <p class="card-text">${producto.descripcion}</p>
+    <p class="card-text">Cantidad agregada:${producto.cantidad}</p>
+    <button type="button" class="btn btn-dark m-3 fs-6" id="btnCardsBorrar${producto.id}">Quitar del carrito</button>
+  </div>
+  </div>
+  `;
+  });
+  quitarDelCarrito();
+}
+
+function quitarDelCarrito() {
+  carritoCompras.forEach((producto) => {
+    document
+      .querySelector(`#btnCardsBorrar${producto.id}`)
+      .addEventListener("click", () => {
+        carritoCompras = carritoCompras.filter(
+          (producto3) => producto3.id !== producto.id
+        );
+        agregarAlCarrito();
+      });
+  });
 }
 
 //Evento Form
