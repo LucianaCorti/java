eb38187df444c592695a42b5ba953e184953b8e8
Luciana Corti <lulicorti19@gmail.com>
2022-08-27 13:17:27 -0300

    agregue boton restar productos

--- js/carrito.js
+++ js/carrito.js
@@ -1,9 +1,9 @@
 let carritoCompras = JSON.parse(localStorage.getItem("carritoCompras")) || [];
 const carritoProductos = document.querySelector(".carritoCompras");
 function agregarAlCarrito() {
-    carritoProductos.innerHTML = "";
-    carritoCompras.forEach((producto) => {
-      carritoProductos.innerHTML += `<div class="card">
+  carritoProductos.innerHTML = "";
+  carritoCompras.forEach((producto) => {
+    carritoProductos.innerHTML += `<div class="card">
       <h5 class="card-title text-decoration-underline m-2">Carrito Compras</h5>
       <img class="card-img-top" src=${producto.foto}>
       <div class="card-body">
@@ -11,26 +11,44 @@ function agregarAlCarrito() {
       <p class="card-text">${producto.descripcion}</p>
       <p class="card-text">Cantidad agregada:${producto.cantidad}</p>
       <button type="button" class="btn btn-dark m-3 fs-6" id="btnCardsBorrar${producto.id}">Quitar del carrito</button>
+      <button type="button" class="btn btn-dark m-3 fs-6" id="btnCardsRestar${producto.id}">Restar</button>
     </div>
     </div>
     `;
-    });
-    localStorage.setItem("carritoCompras", JSON.stringify(carritoCompras));
-  
-    quitarDelCarrito();
-  }
+  });
+  localStorage.setItem("carritoCompras", JSON.stringify(carritoCompras));
+  quitarDelCarrito();
+  funcionBotonesRestar();
+}
+agregarAlCarrito();
+
+function quitarDelCarrito() {
+  carritoCompras.forEach((producto) => {
+    document
+      .querySelector(`#btnCardsBorrar${producto.id}`)
+      .addEventListener("click", () => {
+        carritoCompras = carritoCompras.filter(
+          (producto3) => producto3.id !== producto.id
+        );
+        agregarAlCarrito();
+      });
+  });
+}
+
+function carritoRestar(producto) {
+  let restarProd = carritoCompras.find((prod8) => prod8.id === producto.id);
+  producto.cantidad--;
+
   agregarAlCarrito();
-  
-  function quitarDelCarrito() {
-    carritoCompras.forEach((producto) => {
-      document
-        .querySelector(`#btnCardsBorrar${producto.id}`)
-        .addEventListener("click", () => {
-          carritoCompras = carritoCompras.filter(
-            (producto3) => producto3.id !== producto.id
-          );
-          agregarAlCarrito();
-        });
-    });
-  }
-  
+}
+
+function funcionBotonesRestar() {
+  carritoCompras.forEach((producto) => {
+    document
+      .querySelector(`#btnCardsRestar${producto.id}`)
+      .addEventListener("click", () => {
+        carritoRestar(producto);
+        console.log("click");
+      });
+  });
+}
